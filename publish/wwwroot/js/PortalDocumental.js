6449c01e7e9c230ce9746fc2de3564a5937d2fdd
GerardoAlfonso <Gerardoalfonso_@hotmail.com>
2022-11-23 23:08:34 -0600

    local test

--- publish/wwwroot/js/PortalDocumental.js
+++ publish/wwwroot/js/PortalDocumental.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
     function LoadPath() {
         $.ajax({
             type: "GET",
-            url: getHostName() + "/Files/GetPublicPath",
+            url: getHostName() + "/Files/GetPrivatePath",
             dataType: "json",
             contentType: "Application/json",
             success: function (result) {
                 if (result.status == 1) {
                     debugger;
-                    sessionStorage.setItem("path", result.data[0] + "\\GAMA\\Private");
+                    sessionStorage.setItem("path", result.data[0]);
                 }
                 else {
                     alert(result.message)
@@ -99,9 +99,12 @@ function OpenFolder(name) {
 function GetRootFiles() {
     $.ajax({
         type: "GET",
-        url: getHostName() + "/Files/GetPublicFiles",
+        url: getHostName() + "/Files/GetPrivateFiles",
         dataType: "json",
         contentType: "Application/json",
+        headers: {
+            Authorization: 'Bearer ' + window.localStorage.getItem("token")
+        },
         success: function (result) {
             if (result.status == 1) {
                 PrintFilesAndFolders(result.folders, result.files)
